1cc04ddcebea174cca885cd31aac6e1135c7e180
alweb29 <albertow55@gmail.com>
2023-10-05 19:31:55 +0200

    Added front page of application using view binding

--- app/src/main/java/com/example/alweb29/workoutapp/MainActivity.kt
+++ app/src/main/java/com/example/alweb29/workoutapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.alweb29.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.example.alweb29.workoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding:ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener{
+            Toast.makeText(this, "Start", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
     }
 }
